63eb01be63b877fb20d8db4443597b85a5a9c871
Frances9 <44355133+Frances9@users.noreply.github.com>
2023-04-17 10:23:42 +0800

    Update display_bitmap.py

--- examples/display_bitmap.py
+++ examples/display_bitmap.py
@@ -1,8 +1,8 @@
 # -*- coding:utf-8 -*-
 """!
   @file display_bitmap.py
-  @brief 位图显示
-  @n 实验现象：墨水屏支持显示单色位图的bmp图片，更改该文件中的文件路径和文件名，即可显示你自己的图片。
+  @brief Bitmap display
+  @n Experiment phenomenon: The e-ink screen supports displaying monochrome bitmap BMP images. Change the file path and file name in this file to display your own image.
   @copyright   Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
   @License     The MIT License (MIT)
   @author      fengli(li.feng@dfrobot.com)
